a8e00c82d79b55490edc46c9a73eabb58b19de91
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-16 14:51:04 -0700

    Quote pres component

---
+++ src/components/quotes/Quote.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Quote({ quote }) {
+  return (
+    <section>
+      <img src={quote.img}/>
+      <p>{quote.character}</p>
+      <p>{quote.quote}</p>
+    </section>
+  );
+}
+
+Quote.propTypes = {
+  quote: PropTypes.shape({
+    img: PropTypes.string,
+    character: PropTypes.string,
+    quote: PropTypes.quote
+  }).isRequired
+};
+
+
+export default Quote;
